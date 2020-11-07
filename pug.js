24d593810b544da3c8601ecce8404c05973e3be2
Saul Bustamante <sbustamante@railtronix.com>
2020-11-06 22:35:33 -0600

    Improving pug template

--- pug.js
+++ pug.js
@@ -1,9 +1,34 @@
 const pug = require('pug');
 
-// Compile the source code
-const compiledFunction = pug.compileFile('template.pug');
+let subject = {
+  id : "did:example:ebfeb1f712ebc6f1c276e12ec21",
+  givenName : "Saul",
+  additionalName : "Additional Name",
+  familyName : "Family Name",
+  gender : "Male",
+  honorificPrefix : "PREFIX",
+  honorificSuffix : "SUFFIX",
+  photograph : "photograph"
+};
 
+let fhir = {
+  version : "1.0.0"
+};
+
+let values = {
+  credential_id : "http://example.edu/credentials/1872",
+  issuer_ref : "https://example.edu/issuers/565049",
+  issuance_date : "2010-01-01T19:73:24Z",
+  expiration_date : "2011-01-01T19:73:24Z",
+  subject : subject,
+  fhir : fhir
+};
+
+// Compile the source code
+const compiledFunction = pug.compileFile('./resources/template.pug');
+
+let result = compiledFunction(values);
+
+console.log(result);
 // Render a set of data
-console.log(compiledFunction({
-  name: 'Timothy'
-}));
+//console.log(JSON.parse(result));
